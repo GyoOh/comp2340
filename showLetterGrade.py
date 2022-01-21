99c85519fa0c794bc1aedff8bd703385e2eb0221
Gwanggyo Oh <goh3@my.bcit.ca>
2022-01-20 20:19:32 -0800

    added while loop and fixed the conflict

--- showLetterGrade.py
+++ showLetterGrade.py
@@ -1,5 +1,27 @@
 def showLetterGrade(percentage):
     # end="" avoids pointer going to new line.
+<<<<<< HEAD  
+  if percentage >= 90:
+        grade = 'A'
+        print("The grade " + str(percentage) + " is ", grade)
+    elif percentage >= 80:
+        grade = 'B'
+        print("The grade " + str(percentage) + " is ", grade)
+    elif percentage >= 70:
+        grade = 'C'
+        print("The grade " + str(percentage) + " is ", grade)
+    elif percentage >= 60:
+        grade = 'D'
+        print("The grade " + str(percentage) + " is ", grade)
+    else:
+        grade = 'F'
+        print("The grade " + str(percentage) + " is ", grade)
+    # if-elif-else series goes here inside the function.
+
+showLetterGrade(95)
+showLetterGrade(72)
+showLetterGrade(51)
+======
     if percentage >= 90:
         grade = 'A'
         print("The grade " + str(percentage) + " is ", grade)
@@ -30,3 +52,5 @@ while True:
     else:
         percentage = int(inputGrade)
         showLetterGrade(percentage)
+
+
